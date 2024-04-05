3219003032f67aa806e6c76ebe54037c590f6e0d
VicentePSalcedo <vicentepsalcedo@gmail.com>
2024-04-05 17:56:25 -0400

    update from just script

--- home/nvim/plugin/telescope-nvim.lua
+++ home/nvim/plugin/telescope-nvim.lua
@@ -1,4 +1,16 @@
-nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
-nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
-nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
-nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+require('telescope').setup({
+	extensions = {
+    	fzf = {
+      	fuzzy = true,                    -- false will only do exact matching
+      	override_generic_sorter = true,  -- override the generic sorter
+      	override_file_sorter = true,     -- override the file sorter
+      	case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    	}
+  	}
+})
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
