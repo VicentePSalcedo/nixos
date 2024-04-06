0874b67beec397c3dc87fd820bfdd43c90c9f67b
VicentePSalcedo <vicentepsalcedo@gmail.com>
2024-04-06 18:16:05 -0400

    update from just script

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -1,9 +1,10 @@
+{ pkgs, config, inputs, ...}:
 {
   programs.rofi = {
     enable = true;
     font = "FiraCodeNerdFont";
   };
-  home.file.".config/rofi" = {
+  home.file.".config/rofi/config.rasi" = {
       source = ./config.rasi;
   };
 }
