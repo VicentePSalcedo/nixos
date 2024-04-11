5ce88dcaeccfba1d229694c815fdecb9796072f4
VicentePSalcedo <vicentepsalcedo@gmail.com>
2024-04-11 10:26:16 -0400

    Updated: 2024-04-11 10:26:16

--- modules/grub.nix
+++ modules/grub.nix
@@ -1,13 +1,15 @@
 { config, ... }:
 {
   boot.loader = {
+    grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+      useOSProber = true;
+      configurationLimit = 50;
+    };
     efi = {
       canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/EFI";
-    };
-    grub = {
-      efiSupport = true;
-      device = "nodev";
     };
   };
 }
