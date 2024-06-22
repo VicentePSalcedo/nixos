54e38426c071020abc51b014497e1f1cf9b1ce61
Vicente Salcedo <vicentepsalcedo@gmail.com>
2024-06-22 01:24:32 -0400

    Updated: 2024-06-22 01:24:32

---
+++ diablo/home.nix
@@ -0,0 +1,54 @@
+
+{ config, lib, pkgs, inputs, ... }:
+{
+  imports = [
+    ../home/alacritty
+    ../home/bash
+    ../home/gtk
+    ../home/nvim
+    ../home/polybar
+    ../home/picom
+    ../home/rofi
+  ];
+  home.username = "sintra";
+  home.homeDirectory = "/home/sintra";
+  home.packages = with pkgs; [
+    anki
+    bottom #btm to view resource usage
+    discord-canary
+    discord-screenaudio
+    du-dust #dust to view file usage
+    dunst #handles notifications
+    fira-code-nerdfont
+    firefox
+    flameshot #screen shot app
+    google-chrome
+    just #just Just
+    neofetch
+    playerctl #used to bind media keys
+    pywal #generates colorscheme based off of wallpaper
+    obsidian #note taking app
+    r2modman
+    rofi #application menu
+    rhythmbox #music player
+    slack
+    syncthing
+    xfce.thunar
+    thunderbird
+    tmux
+    yt-dlp #youtube audio downloads
+    # support both 32- and 64-bit applications
+    wineWowPackages.stable
+    winetricks
+  ];
+  services = {
+    syncthing.enable = true;
+    redshift = {
+        enable = true;
+        longitude = -82.114749;
+        latitude = 29.558167;
+    };
+  };
+  home.stateVersion = "23.11";
+  programs.home-manager.enable= true;
+}
--- flake.nix
+++ flake.nix
@@ -41,6 +41,21 @@
           }
         ];
       };
+      diablo = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linx";
+        modules = [
+          ./diablo/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.sintra = import ./diablo/home.nix;
+          }
+          {
+            _module.args = { inherit inputs; };
+          }
+        ];
+      };
     };
   };
 }
