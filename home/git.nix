dbaef52ca1646cba261051fbed40bdc03ee7d394
VicentePSalcedo <vicentepsalcedo@gmail.com>
2024-12-28 21:39:19 -0500

    Updated: 2024-12-28 21:39:19

--- home/git.nix
+++ home/git.nix
@@ -4,6 +4,7 @@
     userEmail = "vicentepsalcedo@gmail.com";
     userName = "VicentePSalcedo";
     signing = {
+      key = "EF6EDD4359F0E184CC4B19C868C579FE4A527CA1";
       signByDefault = true;
     };
   };
