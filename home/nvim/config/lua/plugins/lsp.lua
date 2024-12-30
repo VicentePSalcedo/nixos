460ed6886ea3a5811344ad4dc1ac323e7c3b48e2
VicentePSalcedo <vicentepsalcedo@gmail.com>
2024-12-30 16:03:23 -0500

    Updated: 2024-12-30 16:03:23

---
+++ home/nvim/config/lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        'neovim/nvim-lspconfig',
+        -- IMPORTANT: someone smarter than me got this workign so dont touch
+        -- this is used to enable typsciprt auto completes and highlights
+        lazy = false,
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.angularls.setup({})
+            lspconfig.eslint.setup({})
+            lspconfig.jdtls.setup({})
+            lspconfig.jedi_language_server.setup({})
+            lspconfig.lua_ls.setup({})
+            lspconfig.rust_analyzer.setup({})
+            lspconfig.ts_ls.setup({})
+            lspconfig.nixd.setup({})
+        end,
+        -- IMPORTANT --
+    },
+    { 'mason.nvim',                        enabled = false },
+    { 'williamboman/mason-lspconfig.nvim', enabled = false },
+}
