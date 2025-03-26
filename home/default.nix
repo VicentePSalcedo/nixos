f836784571fa8b2f57b6ccc7178d783d94399265
VicentePSalcedo <vicentepsalcedo@gmail.com>
2025-03-26 13:25:25 -0400

    Updated: 2025-03-26 13:25:25

--- home/default.nix
+++ home/default.nix
@@ -15,27 +15,22 @@
   home.username = "sintra";
   home.homeDirectory = "/home/sintra";
   home.packages = with pkgs; [
-    amberol
     anki-bin # flash card app
     direnv # cause "nix develope" is to much to type everytime
     du-dust # dust to view file usage
     dunst # handles notifications
     feh # most basic bitch image viewer I could find (hope you like keyboard navigation)
-    figma-linux
     gimp
     just # just Just, (yeah I'm lazy)
-    lazygit
     librewolf # becuase you deserve a pain in the a** in the name of internet privacy and security
     networkmanagerapplet # the wifi bars in polybar are thanks to this guy
     nerd-fonts.fira-code # favorite font that support ligatures
     obsidian
-    ranger
     playerctl # used to bind media keys
     postman
     pywal # setting wallpaper and a matching color scheme
     rhythmbox # tried many, still the best minimal music player I know
-    ungoogled-chromium
-    vesktop
+    vscodium
     xfce.thunar # file explorer
     xfce.tumbler # used for thumbnail images inside thunar, might need to change settings inside thunar itself
   ];
