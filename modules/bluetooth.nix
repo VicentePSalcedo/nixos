02dd3dafa27d2729a3ba42ab3ba4b7dd594be76f
VicentePSalcedo <vicentepsalcedo@gmail.com>
2025-03-27 14:59:04 -0400

    Updated: 2025-03-27 14:59:04

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -1,7 +1,11 @@
+{ pkgs, ... }:
 {
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
   };
   services.blueman.enable = true;
+  environment.systemPackages = with pkgs; [
+    nettools
+  ];
 }
