8379ff313a0700c56eb3d1086a0cd5e10d51aa9a
VicentePSalcedo <vicentepsalcedo@gmail.com>
2025-10-14 17:13:31 -0400

    NixOS Gen: 619

--- wraith/configuration.nix
+++ wraith/configuration.nix
@@ -15,6 +15,13 @@
   #     settings.Login.HandleLidSwitchDocked = "ignore";
   #   };
   # };
+
+  powerManagement.enable = true;
+  services.logind.lidSwitch = "hibernate";
+  # Hibernate on power button pressed
+  services.logind.powerKey = "poweroff";
+  boot.resumeDevice = "";
+
   # Pretty sure this labels the encrypted disk. Don't f*** with this until you find out.
   boot.initrd.luks.devices."luks-321cf864-183e-4548-836b-9d8a6ad38559".device =
     "/dev/disk/by-uuid/321cf864-183e-4548-836b-9d8a6ad38559";
