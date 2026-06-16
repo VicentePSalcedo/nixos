3150180cb62e288953be3003d44063556e8a6f8a
Vicente <vicentesalcedo@artoriastechlab.com>
2026-06-16 00:42:25 -0400

    update for wraith

--- hosts/wraith/default.nix
+++ hosts/wraith/default.nix
@@ -1,19 +1,20 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-    ../../system/gaming.nix
-    ../../system/hermes.nix
-    ../../system/sops.nix
-    ../../system/direnv.nix
-    ../../system/docker.nix
-    ../../system/gnupg.nix
-    ../../system/nix-settings.nix
-    ../../system/tmux.nix
-    ../../system/ssh.nix
-    ../../system/tailscale.nix
-  ];
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../../system/gaming.nix
+      ../../system/hermes.nix
+      ../../system/sops.nix
+      ../../system/direnv.nix
+      ../../system/docker.nix
+      ../../system/gnupg.nix
+      ../../system/nix-settings.nix
+      ../../system/tmux.nix
+      ../../system/ssh.nix
+      ../../system/tailscale.nix
+    ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 6;
@@ -21,7 +22,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "phantom";
+  networking.hostName = "wraith";
   networking.networkmanager.enable = true;
 
   time.timeZone = "America/New_York";
@@ -29,12 +30,6 @@
   services.xserver.xkb = {
     layout = "us";
     variant = "";
-  };
-
-  users.users.sintra = {
-    isNormalUser = true;
-    description = "sintra";
-    extraGroups = [ "networkmanager" "wheel" "hermes" ];
   };
 
   programs.hyprland.enable = true;
@@ -47,10 +42,12 @@
     users.sintra = import ../../users/sintra/home.nix;
   };
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
+  users.users."sintra" = {
+    isNormalUser = true;
+    description = "sintra";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
   };
 
-  system.stateVersion = "26.05";
+  system.stateVersion = "26.05"; # Did you read the comment?
 }
