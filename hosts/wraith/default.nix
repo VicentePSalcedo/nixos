79e16c18293543ac2a26a9542adcfe253f2f3590
Vicente <vicentesalcedo@artoriastechlab.com>
2026-06-16 03:44:39 -0400

    NixOS Gen: 14

--- hosts/wraith/default.nix
+++ hosts/wraith/default.nix
@@ -15,4 +15,18 @@
   environment.systemPackages = with pkgs; [
     brightnessctl
   ];
+
+  # Ultra-fast boot optimizations for the laptop
+  boot = {
+    loader.timeout = pkgs.lib.mkForce 0; # Skip the 7-second boot menu (hold space during boot to show it)
+    initrd.systemd.enable = true; # Use parallelized systemd in initrd instead of bash scripts
+    consoleLogLevel = 0;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+  };
 }
