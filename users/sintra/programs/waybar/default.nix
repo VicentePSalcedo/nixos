3ad02e25f19b98ed3c0990291c1f9a47bc0d3614
Vicente <vicentesalcedo@artoriastechlab.com>
2026-06-16 03:52:07 -0400

    NixOS Gen: 15

--- users/sintra/programs/waybar/default.nix
+++ users/sintra/programs/waybar/default.nix
@@ -10,7 +10,7 @@
         height = 24;
         modules-left = [ "hyprland/workspaces" ];
         modules-center = [ "clock" ];
-        modules-right = if osConfig.networking.hostName == "wraith" then [ "battery" ] else [];
+        modules-right = if osConfig.networking.hostName == "wraith" then [ "bluetooth" "battery" ] else [];
         "hyprland/workspaces" = {
           format = "{name}";
           on-click = "activate";
@@ -26,6 +26,15 @@
             warning = 30;
             critical = 15;
           };
+        };
+        "bluetooth" = {
+          format = " {status}";
+          format-connected = " {device_alias}";
+          format-connected-battery = " {device_alias} {device_battery_percentage}%";
+          tooltip-format = "{controller_alias}\t{controller_address}\n\n{num_connections} connected";
+          tooltip-format-connected = "{controller_alias}\t{controller_address}\n\n{num_connections} connected\n\n{device_enumerate}";
+          tooltip-format-enumerate-connected = "{device_alias}\t{device_address}";
+          tooltip-format-enumerate-connected-battery = "{device_alias}\t{device_address}\t{device_battery_percentage}%";
         };
       };
     };
