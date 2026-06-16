bcf16265b62da3229b822354408a9679ed00db1d
Vicente <vicentesalcedo@artoriastechlab.com>
2026-06-16 14:08:27 -0400

    NixOS Gen: 17

--- users/sintra/programs/git.nix
+++ users/sintra/programs/git.nix
@@ -3,6 +3,9 @@
 {
   programs.git = {
     enable = true;
+    extraConfig = {
+      pull.rebase = true;
+    };
     # Add your default config if desired, e.g.:
     # userName = "Sintra";
     # userEmail = "sintra@example.com";
