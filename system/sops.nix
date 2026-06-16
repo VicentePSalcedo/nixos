79b3d71e6bde9862415beb6a172ec03ae52ef4e6
Vicente <vicentesalcedo@artoriastechlab.com>
2026-06-16 14:31:15 -0400

    update

--- system/sops.nix
+++ system/sops.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 {
-  sops = lib.mkIf (config.networking.hostName == "phantom") {
+  sops = {
     defaultSopsFile = ../secrets.yaml;
     age.keyFile = "/home/sintra/.config/sops/age/keys.txt";
     secrets."hermes-env" = { format = "yaml"; };
