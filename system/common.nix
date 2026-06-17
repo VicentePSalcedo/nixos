b9982e054357a14a6611319eac4215bd62ea6210
Vicente <vicentesalcedo@artoriastechlab.com>
2026-06-17 00:49:02 -0400

    NixOS Gen: 44

--- system/common.nix
+++ system/common.nix
@@ -16,7 +16,7 @@
 
   
   # Ensure just is installed system-wide on all hosts now and in the future
-  environment.systemPackages = [ pkgs.just ];
+  environment.systemPackages = with pkgs; [ just rqbit ];
 
   # Bootloader configurations
   boot.loader.systemd-boot.enable = true;
