2f2de793dd71000d7f4625a41c916a137b40c82b
Vicente <vicentesalcedo@artoriastechlab.com>
2026-06-16 23:46:28 -0400

    NixOS Gen: 41

--- hosts/wraith/hardware-configuration.nix
+++ hosts/wraith/hardware-configuration.nix
@@ -24,7 +24,7 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [ { device = "/var/lib/swapfile"; size = 16384; } ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
