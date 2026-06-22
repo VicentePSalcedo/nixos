760a733baec4e49025265521e56bf0c39b52eb57
Vicente <vicentesalcedo@artoriastechlab.com>
2026-06-22 11:35:33 -0400

    NixOS Gen: 63 (pre-switch)

--- users/sintra/programs/ghostty.nix
+++ users/sintra/programs/ghostty.nix
@@ -7,6 +7,7 @@
       font-size = 11;
       background-opacity = 0.40;
       background-blur = false;
+      confirm-close-surface = false;
       background = "#000000";
       theme = "TokyoNight Storm";
       command = "${pkgs.nushell}/bin/nu"; # Launch Nushell by default
