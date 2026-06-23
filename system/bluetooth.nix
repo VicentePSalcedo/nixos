4a048aeb1b298b318e59ba3824b9b17354ac5410
Vicente <vicentesalcedo@artoriastechlab.com>
2026-06-22 20:52:23 -0400

    NixOS Gen: 72 (pre-switch)

--- system/bluetooth.nix
+++ system/bluetooth.nix
@@ -4,6 +4,13 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+        Experimental = true;
+        Class = "0x000100"; # Announce computer/audio profile capability to connecting devices
+      };
+    };
   };
   # services.blueman.enable = true;
 }
