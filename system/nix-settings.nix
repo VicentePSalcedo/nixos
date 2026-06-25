356d506504123886139600bedc4ef12634b1b11c
Vicente Salcedo <vicentepsalcedo@gmail.com>
2026-06-24 22:00:22 -0400

    NixOS Gen: 80 (pre-switch)

--- system/nix-settings.nix
+++ system/nix-settings.nix
@@ -8,7 +8,13 @@
       trusted-users = [ "sintra" ];
     };
   };
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config = {
+    allowUnfree = true;
+    permittedInsecurePackages = [
+      "librewolf-151.0.2-1"
+      "librewolf-unwrapped-151.0.2-1"
+    ];
+  };
 
   nixpkgs.overlays = [
     inputs.firefox-addons.overlays.default
