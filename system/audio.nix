7ddaee228f69e508dfa9a836308f5dbe2a0f8293
Vicente Salcedo <vicentepsalcedo@gmail.com>
2026-07-17 11:05:18 -0400

    NixOS Gen: 151 (pre-switch)

--- system/audio.nix
+++ system/audio.nix
@@ -11,6 +11,11 @@
     # and the internal speaker has a rock-bottom priority
     wireplumber.extraConfig = {
       "10-audio-priorities" = {
+        "wireplumber.settings" = {
+          # Don't remember/promote previously-selected default, so newly
+          # connected devices (headphones) can win based on priority alone
+          "node.restore-default-targets" = false;
+        };
         "monitor.alsa.rules" = [
           {
             matches = [
@@ -23,6 +28,22 @@
                 # Rock-bottom priority so it is never auto-selected over Bluetooth
                 "priority.driver" = 100;
                 "priority.session" = 100;
+              }
+            }
+          }
+          # Give wired headphones/headsets a priority above the internal
+          # speaker (100) but below Bluetooth (2000), so they auto-switch
+          # when plugged in
+          {
+            matches = [
+              {
+                "device.form-factor" = "~headphone|headset|portable";
+              }
+            ];
+            actions = {
+              update-props = {
+                "priority.driver" = 1500;
+                "priority.session" = 1500;
               };
             };
           }
