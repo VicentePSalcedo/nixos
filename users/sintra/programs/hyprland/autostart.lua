11719cfc1321a25b8a53b437ba46a7dd1b7ce71d
Vicente Salcedo <vicentepsalcedo@gmail.com>
2026-08-05 14:03:43 -0400

    Revert "NixOS Gen: 168 (pre-switch)"
    This reverts commit 78fd4378a7978bafba0476376bf42dda59d2d380.

--- users/sintra/programs/hyprland/autostart.lua
+++
@@ -1,9 +0,0 @@
--- Autostart applications
-hl.on("hyprland.start", function()
-    hl.exec_cmd("foot --server")
-    hl.exec_cmd("swaybg -i /home/sintra/nixos/wallpapers/tokyonight.png -m fill")
-    hl.exec_cmd("waybar")
-    hl.exec_cmd("mako")
-    hl.exec_cmd("/home/sintra/nixos/users/sintra/programs/hyprland/handle-resume.sh")
-    hl.exec_cmd("protonvpn-app")
-end)
