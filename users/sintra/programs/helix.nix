fd2b246a5e1f17643756b6872ca90f3e7f08becd
Vicente Salcedo <vicentepsalcedo@gmail.com>
2026-08-11 17:38:38 -0400

    NixOS Gen: 177 (pre-switch)

--- users/sintra/programs/helix.nix
+++ users/sintra/programs/helix.nix
@@ -4,6 +4,8 @@
   programs.helix = {
     enable = true;
     defaultEditor = true;
+    # marksman: markdown LSP (wikilinks, backlinks, headings) - scoped to helix's PATH only
+    extraPackages = [ pkgs.marksman ];
     settings = {
       theme = "tokyonight_storm_transparent";
       editor = {
