8a882b8940d46f4dd73a82d381ddb514512d8b26
Vicente Salcedo <vicentepsalcedo@gmail.com>
2026-08-17 17:09:49 -0400

    NixOS Gen: 187 (pre-switch)

--- users/sintra/programs/beets.nix
+++ users/sintra/programs/beets.nix
@@ -8,7 +8,7 @@
       directory = "/home/sintra/Music/Library";
       library = "/home/sintra/Music/Library/musiclibrary.db";
       ignore = [ ".*" "*~" "System Volume Information" "lost+found" "Library" ];
-      plugins = "scrub chroma lastgenre duplicates spotify inline";
+      plugins = "chroma lastgenre duplicates spotify inline";
       threaded = false;
       
       item_fields = {
